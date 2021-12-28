ab8dbdf4bbb96d2a4011522f3de8f62c063db802
Gabril <gjperes1@gmail.com>
2021-12-28 16:51:15 -0300

    'refatorado' - mudança de nome de variaveis e um pouco do funcionamento

--- score-keeper/app.js
+++ score-keeper/app.js
@@ -1,22 +1,44 @@
-const player01 = document.querySelector("#pontos-01")
-const player02 = document.querySelector("#pontos-02")
+const p1Display = document.querySelector("#p1Display")
+const p2Display = document.querySelector("#p2Display")
 
-const addPontoPlayer01 = document.querySelector("#score-01")
-const addPontoPlayer02 = document.querySelector("#score-02")
+const p1Btn = document.querySelector("#p1Btn")
+const p2Btn = document.querySelector("#p2Btn")
 
-addPontoPlayer01.addEventListener("click", function () {
-    const pontosPlayer01 = parseInt(player01.innerText)
-    player01.innerText = pontosPlayer01 + 1
+const restart = document.querySelector('#restart')
+
+let maxPontos = parseInt(document.querySelector("#maxPontos").value)
+let p1Pontos = 0
+let p2Pontos = 0
+let isGameOver = false
+
+// Incrementa pontos do player 01
+p1Btn.addEventListener("click", function () {
+    if(!isGameOver)
+        p1Pontos++
+        if(p1Pontos === maxPontos)
+            isGameOver = true
+        p1Display.textContent = p1Pontos
 })
 
-addPontoPlayer02.addEventListener("click", function () {
-    const pontosPlayer02 = parseInt(player02.innerText)
-    player02.innerText = pontosPlayer02 + 1
+// Incrementa pontos do player 02
+p2Btn.addEventListener("click", function () {
+    if(!isGameOver)
+        p2Pontos++
+        if(p2Pontos === maxPontos)
+            isGameOver = true
+        p2Display.textContent = p2Pontos
 })
 
-const restartBtn = document.querySelector('#restart')
+// Reinicia o placar
+restart.addEventListener("click", function() {
+    p1Display.textContent = 0
+    p1Pontos = 0
+    p2Display.textContent = 0
+    p2Pontos = 0
+    isGameOver = false
+})
 
-restartBtn.addEventListener("click", function() {
-    player01.innerText = 0
-    player02.innerText = 0
+// Redefine o máximo de pontos possível
+document.querySelector("#maxPontos").addEventListener("change", function() {
+    maxPontos = parseInt(document.querySelector("#maxPontos").value)
 })
