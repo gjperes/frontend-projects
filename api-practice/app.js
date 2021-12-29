85b9d638f29fec23c09bfd10fab05b6646891fb4
Gabril <gjperes1@gmail.com>
2021-12-28 23:58:28 -0300

    apenas o básico

---
+++ api-practice/app.js
@@ -0,0 +1,29 @@
+const btn = document.querySelector("button")
+const ul = document.querySelector(".waifus")
+
+const createWaifu = (url) => {
+    const novaWaifu = document.createElement('img')
+    novaWaifu.src = url
+    return novaWaifu
+}
+
+const novoLi = (img) => {
+    const li = document.createElement('li')
+    li.append(img)
+    return li
+}
+
+const getWaifu = async () => {
+    const url = "https://api.waifu.pics/sfw/waifu"
+    try {
+        const res = await fetch(url)
+        const data = await res.json()
+        ul.append( novoLi( createWaifu(data.url) ) )
+    } catch (error) {
+        console.log("Deu treta", error)
+    }
+}
+
+btn.addEventListener("click", function() {
+    getWaifu()
+})
