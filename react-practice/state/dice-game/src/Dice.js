86ea681d6f21dfcc36da8ca8ac923df48ab858e9
Gabril <gjperes@ufpr.br>
2022-03-17 17:09:55 -0300

    dice renders correct icon

--- react-practice/state/dice-game/src/Dice.js
+++ react-practice/state/dice-game/src/Dice.js
@@ -3,9 +3,19 @@ import './Dice.css';
 
 class Dice extends Component {
     render() {
+        const { value } = this.props;
+        const numberText = {
+            1: "one",
+            2: "two",
+            3: "three",
+            4: "four",
+            5: "five",
+            6: "six"
+        }
+
         return (
             <div>
-                <i className="fa-solid fa-dice-one Dice-ico"/>
+                <i className={"fa-solid fa-dice-" + numberText[value] + " Dice-ico"} />
             </div>
         );
     }
