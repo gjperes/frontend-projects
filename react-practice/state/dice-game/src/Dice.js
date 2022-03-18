6282a82636ceeb0dcb184df4cd8eb8dbf32361ea
Gabril <gjperes@ufpr.br>
2022-03-18 12:11:30 -0300

    boolean operator improvement

--- react-practice/state/dice-game/src/Dice.js
+++ react-practice/state/dice-game/src/Dice.js
@@ -8,7 +8,7 @@ class Dice extends Component {
         return (
             <div className="Dice">
                 <i className={`fa-solid fa-dice-${numberText[this.props.value]}
-                              ${this.props.rolling ? 'Dice-rolling' : ''}`} />
+                              ${this.props.rolling && 'Dice-rolling'}`} />
             </div>
         );
     }
