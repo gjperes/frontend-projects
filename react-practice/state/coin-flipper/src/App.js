ccf6bf690f3aa87ad46d90b449115433dea67641
Gabril <gjperes@ufpr.br>
2022-03-19 11:45:41 -0300

    coin-flipper initial commit

---
+++ react-practice/state/coin-flipper/src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import logo from './logo.svg';
+import './App.css';
+import Coin from "./Coin";
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <Coin />
+          </header>
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ react-practice/state/coin-flipper/src/Coin.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class Coin extends Component {
+    static defaultProps = {
+        side: "head",
+    }
+
+    render() {
+        return (
+            <p>Teste</p>
+        );
+    }
+}
+
+export default Coin;
